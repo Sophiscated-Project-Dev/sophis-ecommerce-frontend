e0dc5699efe03742e4b00d92a8540890b22710ed
Oshaba Samson <chuksdsilent@gmail.com>
2022-06-30 14:39:00 +0100

    Removed the header and footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Layout = props => {
   return (
     <div>
-      {/* -------------------Header---------------- */}
       Header{props.children}
-      {/* -------------------Footer---------------- */}
       Footer
     </div>
   );
