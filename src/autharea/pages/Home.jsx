1c3acb64f8ece0859cc4822be513d09e0381b3c9
Oshaba Samson <chuksdsilent@gmail.com>
2022-06-30 14:35:45 +0100

    created Home layout

--- src/autharea/pages/Home.jsx
+++ src/autharea/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>{/* -------------------Carousel---------------- */}</div>
+
+      <div>{/* -------------------Recommended---------------- */}</div>
+
+      <div>{/* -------------------Flash Sales---------------- */}</div>
+
+      <div>{/* -------------------New Arrivals---------------- */}</div>
+
+      <div>{/* -------------------Top Ranked---------------- */}</div>
+
+      <div>{/* -------------------Brands---------------- */}</div>
+
+      <div>{/* -------------------Client Reviews---------------- */}</div>
+
+      <div>{/* -------------------What we offer---------------- */}</div>
+    </div>
+  );
 };
 
 export default Home;
