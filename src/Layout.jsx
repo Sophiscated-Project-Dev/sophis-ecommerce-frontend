305f92c526f0471b385177ec3b9c20857099bd77
ccanyanwu <eme42c@gmail.com>
2022-06-30 21:55:01 +0100

    feat: add footer component

---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import Footer from "./autharea/components/Footer";
+
+const Layout = (props) => {
+  return (
+    <div>
+      {/* -------------------Header---------------- */}
+      Header{props.children}
+      {/* -------------------Footer---------------- */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
