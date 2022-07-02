b0c62fd724257d7aa09996a8c796615f3a594d3e
Ismail <adegbiteismail27@gmail.com>
2022-07-02 00:27:53 -1100

    added recommended products component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import Footer from "./autharea/components/Footer";
 import Header from "./autharea/components/Header";
+import RecomProds from "./autharea/components/RecomProds";
 
 const Layout = props => {
   return (
     <div>
       <Header />
       {/* -------------------Header---------------- */}
+      <RecomProds/>
       {props.children}
       {/* -------------------Footer---------------- */}
       <Footer />
