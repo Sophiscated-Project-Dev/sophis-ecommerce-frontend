cece8a3109699bb2044d6c3290b4736b45d672dc
Sheila-a <ugwuodo20@gmail.com>
2022-07-02 12:12:40 +0100

    Carousel Commit 1

--- src/autharea/components/Carousel.jsx
+++ src/autharea/components/Carousel.jsx
@@ -1,18 +1,78 @@
 import React from "react";
 import "../styles/Carousel.css";
-import Hero from "../../assets/images/hero-image.png";
+import HeroImg from "../../assets/images/hero-image.png";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Carousel() {
+function Hero() {
   return (
     <div className="hero">
-      <img src={Hero} alt="" className="hero-img" />
-      <div className="hero-text">
-        <h1>Best Gadget Deals</h1>
-        <p className="hero-p1">Up to 70% Off!</p>
-        <button className="hero-btn">Shop Now</button>
-      </div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img className="d-block w-100 gg" src={HeroImg} alt="First slide" />
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off! </p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Second slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off! </p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Third slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off!</p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Fourth slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off!</p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Fifth slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off!</p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Sixth slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off!</p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={HeroImg} alt="Seventh slide" />
+
+          <Carousel.Caption className="hero-text">
+            <h3>Best Gadgets Deals</h3>
+            <p className="hero-p1">Up to 70% Off!</p>
+            <button className="hero-btn">Shop Now </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
 
-export default Carousel;
+export default Hero;
