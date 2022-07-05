2c8722dced606cdc12ea8d78cf7a5a4e26d6c64b
unknown <adegbiteismail27@gmail.com>
2022-07-04 20:01:16 -0700

    moved bootstrap import lines to the top of the file so that they can be overriden by custom css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./autharea/pages/Home";
 import Login from "./noautharea/pages/Login";
 import reportWebVitals from "./reportWebVitals";
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 import Layout from './Layout';
 import Register from './noautharea/pages/Register';
