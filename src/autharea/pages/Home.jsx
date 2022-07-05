dedd609ed1db5b1594d635c62518ac6cff4105b3
unknown <adegbiteismail27@gmail.com>
2022-07-04 21:27:37 -0700

    added New Arrivals component

--- src/autharea/pages/Home.jsx
+++ src/autharea/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import RecomProds from "../components/RecomProds";
+import NewArrivals from "../components/NewArrivals";
+
 const Home = () => {
   return (
     <div>
@@ -9,7 +11,7 @@ const Home = () => {
 
       <div>{/* -------------------Flash Sales---------------- */}</div>
 
-      <div>{/* -------------------New Arrivals---------------- */}</div>
+      <NewArrivals/>
 
       <div>{/* -------------------Top Ranked---------------- */}</div>
 
