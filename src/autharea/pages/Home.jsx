24a2a95a4bba47bbebf48d4f5206ecdc586e231c
unknown <adegbiteismail27@gmail.com>
2022-07-16 08:37:43 -0700

    imported Top Ranked Product Component

--- src/autharea/pages/Home.jsx
+++ src/autharea/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import RecomProds from "../components/RecomProds";
 import NewArrivals from "../components/NewArrivals";
+import TopProducts from "../components/TopProducts";
 import Hero from "../components/Carousel";
 import Review from "../components/Review";
 import Offers from "../components/Offers";
 import "../styles/Home.css";
-import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
@@ -16,7 +16,7 @@ const Home = () => {
 
       <NewArrivals />
 
-      <div>{/* -------------------Top Ranked---------------- */}</div>
+      <TopProducts/>
 
       <div>{/* -------------------Brands---------------- */}</div>
 
