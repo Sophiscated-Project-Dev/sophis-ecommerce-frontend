dbe44f9e652beb681538d8dd22f18a5f9cebc740
unknown <adegbiteismail27@gmail.com>
2022-07-18 14:36:28 -0700

    modified to accept children for reusability

--- src/autharea/components/SectionHeader.jsx
+++ src/autharea/components/SectionHeader.jsx
@@ -1,12 +1,13 @@
 import { BsArrowRight } from "react-icons/bs"
-const SectionHeader = ({ title }) => {
+const SectionHeader = (props) => {
     return (
         <div className="recom-header p-3 mb-0">
-            <h4>{title}</h4>
+            <h4>{props.title}</h4>
             <span className='btn see-all' to="/">
                 <span>see all items</span>
                 <BsArrowRight/>
             </span>
+            {props.children}
         </div>
     )
 }
